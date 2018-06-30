c73d2beaa7a0472cdf015747cab182a2a481d86d
yagqfhd <yagqfhd@126.com>
2018-06-30 15:07:04 +0800

    up

---
+++ School/GanGao.Component.Data/EFDbContext.cs
@@ -0,0 +1,56 @@
+﻿// 源文件头信息：
+// <copyright file="EFDbContext.cs">
+// Copyright(c)2012-2013 GanGaoCN.All rights reserved.
+// CLR版本：4.0.30319.239
+// 开发组织：郭明锋@中国
+// 公司网站：http://www.GanGaocn.net
+// 所属工程：GanGao.Component.Data
+// 最后修改：郭明锋
+// 最后修改：2013/06/14 22:57
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+
+using GanGao.Component.Tools;
+
+
+namespace GanGao.Component.Data
+{
+    /// <summary>
+    ///     EF数据访问上下文
+    /// </summary>
+    [Export(typeof (DbContext))]
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base("default") { }
+
+        public EFDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString) { }
+
+        [ImportMany(typeof(IEntityMapper))]
+        public IEnumerable<IEntityMapper> EntityMappers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            if (EntityMappers == null)
+            {
+                //return;
+                throw PublicHelper.ThrowDataAccessException("实体映射对象个数为0，创建DbContext上下文对象失败。");
+            }
+
+            foreach (var mapper in EntityMappers)
+            {
+                mapper.RegistTo(modelBuilder.Configurations);
+            }
+        }
+    }
+}
---
+++ School/GanGao.Component.Data/EFRepositoryContext.cs
@@ -0,0 +1,39 @@
+﻿// 源文件头信息：
+// <copyright file="EFRepositoryContext.cs">
+// Copyright(c)2012-2013 GanGaoCN.All rights reserved.
+// CLR版本：4.0.30319.239
+// 开发组织：郭明锋@中国
+// 公司网站：http://www.GanGaocn.net
+// 所属工程：GanGao.Component.Data
+// 最后修改：郭明锋
+// 最后修改：2013/06/14 23:06
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+
+namespace GanGao.Component.Data
+{
+    /// <summary>
+    ///     数据单元操作类
+    /// </summary>
+    [Export(typeof (IUnitOfWork))]
+    internal class EFRepositoryContext : UnitOfWorkContextBase
+    {
+        /// <summary>
+        ///     获取 当前使用的数据访问上下文对象
+        /// </summary>
+        protected override DbContext Context
+        {
+            get { return EFDbContext; }
+        }
+
+        [Import(typeof (DbContext))]
+        private EFDbContext EFDbContext { get; set; }
+    }
+}
